abed9a8a7c71c0a1bb50dfdf9f9a7142a7f6c354
jonathlan <38994980+jonathlan@users.noreply.github.com>
2020-11-21 23:26:56 +0100

    Some web servers send N/E in the response, remove it as we don't need it for our tests.

--- myapp.rb
+++ myapp.rb
@@ -18,8 +18,11 @@
         body = open(targeturl) {|f|
         content_type = f.content_type
         f.read
-        }    
+        }
+        # Change date format from DD/MM/YYYY to YYYY-MM-DD
         body.gsub!(/([0-9][0-9])\/([0-9][0-9])\/([0-9][0-9][0-9][0-9])/, "\\3-\\2-\\1")
+        # Some web servers send N/E in the response, remove it as we don't need it for our tests.
+        body.gsub!(/(N\/E)/, "0.0")
         body << ORS
         type = "#{content_type}; charset = UTF-8"
         halt 200, {'Content-Type' => type, 'Content-length' => body.length}, body
