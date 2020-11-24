252bfef58b67fbaf45f0a9f424cfee27f0d14a5c
jonathlan <38994980+jonathlan@users.noreply.github.com>
2020-11-24 21:36:42 +0100

    Add more tracing for heroku

--- myapp.rb
+++ myapp.rb
@@ -11,6 +11,7 @@
         ORS = "\r\n"
         content_type = ''
         requrl = request.url
+        puts "requrl>" << requrl
         
         # Get the base URL
         base = requrl.slice(/(https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\/format\/)/)
@@ -26,16 +27,16 @@
             body = f.read
         }
         # Change date format from DD/MM/YYYY to YYYY-MM-DD
-        puts "body gsub " << body.length.to_s
+            puts "body gsub " << body.length.to_s
         body.gsub!(/([0-9][0-9])\/([0-9][0-9])\/([0-9][0-9][0-9][0-9])/, "\\3-\\2-\\1")
-        puts "body date " << body.length.to_s
+            puts "body date " << body.length.to_s
         # Some web servers send N/E in the response, remove it as we don't need it for our tests.
         body.gsub!(/(N\/E)/, "0.0")
-        puts "body N/E " << body.length.to_s
+            puts "body N/E " << body.length.to_s
         body << ORS
-        puts "body N/E " << body.length.to_s
+            puts "body N/E " << body.length.to_s
         type = "#{content_type}; charset = UTF-8"
-        puts "Type"
+            puts "Type"
         halt 200, {'Content-Type' => type, 'Content-length' => body.length}, body
     end
 
@@ -57,4 +58,9 @@
             content_type = f.content_type
             body = f.read
         }
+        # Change date format from DD/MM/YYYY to YYYY-MM-DD
+            puts "body gsub " << body.length.to_s
+        body.gsub!(/([0-9][0-9])\/([0-9][0-9])\/([0-9][0-9][0-9][0-9])/, "\\3-\\2-\\1")
+            puts "body date " << body.length.to_s
+        body
     end
