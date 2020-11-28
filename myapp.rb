8ce3645f20bc2d89e7b0bf4830745929e492dd34
jonathlan <38994980+jonathlan@users.noreply.github.com>
2020-11-28 23:59:19 +0100

    Do first set of refactoring

--- myapp.rb
+++ myapp.rb
@@ -2,6 +2,7 @@
     require 'sinatra'
     require 'open-uri'
     require 'rdiscount'
+    require_relative 'Formatter'
 
     set :views, settings.root
    
@@ -39,32 +40,19 @@
 
 
     get '/formatr/*' do        
-        ORS = "\r\n"
-        content_type = ''
-        requrl = request.url
+        Format = Formatter.new(request.url)
+
+        if (Format.targer_url != nil)
+            body = Format.get_body(Format.targer_url)
+        else
+            # URL might be wrong
+        end
         
-        # Get the base URL
-        base = requrl.slice(/(https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\/formatr\/)/)
-        rsize = requrl.size
-        # Get the actual target URL that's embeded in request.url, that's after /format/
-        targeturl = requrl.slice(base.size,rsize)
-        # Patch to put missing slash in 'http:/' this means that only secure addresses are supported
-        targeturl.gsub!(/https?:\//,"https://")
-        body = ''
-        URI.open(targeturl) {|f|
-            content_type = f.content_type
-            body = f.read
-        }
-        # Change date format from DD/MM/YYYY to YYYY-MM-DD
-            puts "body gsub " << body.length.to_s
-        body.gsub!(/([0-9][0-9])\/([0-9][0-9])\/([0-9][0-9][0-9][0-9])/, "\\3-\\2-\\1")
-            puts "body date " << body.length.to_s
-        # Some web servers send N/E in the response, remove it as we don't need it for our tests.
-        body.gsub!(/(N\/E)/, "0.0")
-            puts "body N/E " << body.length.to_s
-        body << ORS
-            puts "body ORS " << body.length.to_s
-        type = "#{content_type}; charset = UTF-8"
-            puts "Type"
-        body
+        if (body != nil)
+            # Format body
+            type = "#{body.content_type}; charset = UTF-8"
+            halt 200, {'Content-Type' => type, 'Content-length' => body.size.to_s}, body.body
+        else
+            #Body.result
+        end
     end
